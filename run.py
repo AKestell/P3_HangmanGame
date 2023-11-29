333154449fa163e723b38d609b257fd059df3247
AKestell <>
2023-11-29 22:21:00 +0000

    Set up game and basic tests to make sure it functions.

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import ramdom
+import random
 
 class HangmanGame:
     def __init__(self):
@@ -11,4 +11,18 @@ class HangmanGame:
         self.max_attempts = 6
 
     def choose_film(self):
-        films = []
+        films = ["titanic", "jurassic park", "the matrix", "forrest gump", "pulp fiction",
+                 "braveheart", "the silence of the lambs", "schindlers list", "the lion king",
+                 "saving private ryan", "a few good men", "fight club", "dances with wolves",
+                 "the shawshank redemption", "home alone", "the fifth element", "the truman show",
+                 "goodfellas", "the green mile", "my cousin vinny", "american history x", "the fugitive",
+                 "fargo", "the sixth sense", "the big lebowski", "heat", "groundhog day", 
+                 "edward scissorhands", "pretty woman",]
+        return random.choice(films)
+
+game = HangmanGame()
+
+print(game.film_to_guess)
+print(game.guessed_letters)
+print(game.incorrect_guesses)
+print(game.max_attempts)
