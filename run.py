a93707aa18dc644ccffc8bfb5c31021ac921a1fb
AKestell <>
2023-11-29 22:02:53 +0000

    Initial commit and setup of the game structure

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import ramdom
+
+class HangmanGame:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.film_to_guess = self.choose_film()
+        self.guessed_letters = []
+        self.incorrect_guesses = 0
+        self.max_attempts = 6
+
+    def choose_film(self):
+        films = []
