37767dbef9944fb536f875d13b7165f7ab587d5b
AKestell <>
2023-11-30 21:27:44 +0000

    Added code for the hangman figure.

--- run.py
+++ run.py
@@ -29,6 +29,65 @@ class HangmanGame:
                 display += "_"
         return display
 
+    def hangman_graphic(self):
+        if self.incorrect_guesses == 0:
+            print("________      ")
+            print("|      |      ")
+            print("|             ")
+            print("|             ")
+            print("|             ")
+            print("|             ")
+        elif self.incorrect_guesses == 1:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|             ")
+            print("|             ")
+            print("|             ")
+        elif self.incorrect_guesses == 2:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|     /       ")
+            print("|             ")
+            print("|             ")
+        elif self.incorrect_guesses == 3:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|     /|      ")
+            print("|             ")
+            print("|             ")
+        elif self.incorrect_guesses == 4:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|     /|\     ")
+            print("|             ")
+            print("|             ")
+        elif self.incorrect_guesses == 5:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|     /|\     ")
+            print("|     /       ")
+            print("|             ")
+        else:
+            print("________      ")
+            print("|      |      ")
+            print("|      0      ")
+            print("|     /|\     ")
+            print("|     / \     ")
+            print("|             ")
+            print("The noose tightens around your neck, and you feel the")
+            print("sudden urge to urinate.")
+            print("GAME OVER!")
+            self.reset()
+
+
+
+
 game = HangmanGame()
 
-print(game.display_film())
+
+
